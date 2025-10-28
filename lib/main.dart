0efcc7bd6eb7aa5d67bf32438f973e1caf410dde
Daneshwaran M <daneshwaranm@gmail.com>
2025-10-28 18:26:19 +0530

    Cleaned

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'presentation/pages/create_house.dart';
-import 'presentation/pages/house_list.dart';
-import 'application/bloc/house_bloc.dart';
-import 'infrastructure/repositories/house_repository_impl.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,52 +16,43 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late final HouseBloc _houseBloc;
-
   @override
   void initState() {
     super.initState();
-    _houseBloc = HouseBloc(houseRepository: HouseRepositoryImpl());
-    // Load houses when the app starts
-    _houseBloc.add(const GetAllHousesEvent());
   }
 
   @override
   void dispose() {
-    _houseBloc.close();
     super.dispose();
   }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return BlocProvider.value(
-      value: _houseBloc,
-      child: MaterialApp(
+    return MaterialApp(
+      title: 'Rently',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(
         title: 'Rently',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // TRY THIS: Try running your application with "flutter run". You'll see
-          // the application has a purple toolbar. Then, without quitting the app,
-          // try changing the seedColor in the colorScheme below to Colors.green
-          // and then invoke "hot reload" (save your changes or press the "hot
-          // reload" button in a Flutter-supported IDE, or press "r" if you used
-          // the command line to start the app).
-          //
-          // Notice that the counter didn't reset back to zero; the application
-          // state is not lost during the reload. To reset the state, use hot
-          // restart instead.
-          //
-          // This works for code too, not just values: Most code changes can be
-          // tested with just a hot reload.
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        ),
-        home: const MyHomePage(
-          title: 'Rently',
-          subtitle: 'Manage your properties and tenants',
-        ),
+        subtitle: 'Manage your properties and tenants',
       ),
     );
   }
@@ -91,12 +77,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void _navigateToCreateHouse() {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => const CreateHousePage()),
-    );
-  }
+  void _navigateToCreateHouse() {}
 
   @override
   Widget build(BuildContext context) {
@@ -123,13 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      body: const Column(children: [Expanded(child: HouseListWidget())]),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _navigateToCreateHouse,
-        tooltip: 'Add House',
-        child: const Icon(Icons.add, color: Colors.white),
-        backgroundColor: Color.fromARGB(255, 160, 106, 79),
-      ),
+      body: const Column(children: [Expanded(child: Text('Hello'))]),
     );
   }
 }
